2032e303f7d123191774c68b0065fc834a31a336
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-09-02 17:55:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 class Formatter {
   //add static methods here
-}
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(string) {
+    let exceptions = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    let result = [];
+    let newArray = string.split(" ");
+    for (let n = 0; n < newArray.length; n++) {
+      if (n == 0) {
+        result.push(this.capitalize(newArray[n]));
+      } else {
+        if (exceptions.includes(newArray[n])) {
+          result.push(newArray[n]);
+        } else {
+          result.push(this.capitalize(newArray[n]));
+        }
+      }
+    }
+    return result.join(" ");
+  }
+} // end of class
